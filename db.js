7342c7fe2b521014b3129055833638f1a1617b7c
Abdullah Haroon <burhanharoon@Burhan.local>
2026-05-08 11:52:26 +0500

    Updated database configuration to use environment variables, added dotenv for environment variable management, and expanded .gitignore to include database and log files.

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("calculator_db", "burhanharoon", null, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD || null,
+  {
     // new Sequelize(database, username, password, options)
-  host: "localhost",
-  dialect: "postgres",
-});
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+);
 
 export default sequelize;
